2e973e273828ed1563179d9a0559a01b61c81472
SangSai-10 <sangsai2010@gmail.com>
2026-01-10 14:53:31 -0500

    Update excercise2.py

--- excercise2.py
+++ excercise2.py
@@ -1,3 +1,4 @@
+# Given age check if user can vote ex age>18
 def can_vote(age):
     result = age > 18
     return result
@@ -11,20 +12,24 @@ eligible_to_vote  = can_vote(age)
 print(" User eligible to vote:?" , eligible_to_vote)
 print("_______________________________________________")
 
-def drive(age,license):
-    result = age > 18 and license == False
+# Given age check if user can drive 
+def drive(age,has_license):
+    result = age > 18 and has_license == True
     return result
 
 age = 15 
-license = False
-eligible_to_drive  = drive(age,license)
-print(" User eligible to drive:?" , eligible_to_drive)
+has_license = True
+eligible_to_drive  = drive(age,has_license)
+print(" User eligible to drive:?" ,eligible_to_drive)
 
 age = 20
-license = False
-eligible_to_drive = drive(age,license)
-print(" User eligible to drive:?" , eligible_to_drive)
+has_license = True
+eligible_to_drive = drive(age,has_license)
+print(" User eligible to drive:?" ,eligible_to_drive)
 print("_______________________________________________")
+
+# Given score check
+#  whether candidate passed, failed, scored centum
 
 def student_score(marks):
     pass_fail = marks >= 35
@@ -33,7 +38,6 @@ def student_score(marks):
 marks = 40
 exam_result = student_score(marks)
 print("Student score pass mark:?", exam_result)
-print("Student score fail mark:?", not exam_result)
 
 def student_score(marks):
     pass_fail = marks < 34
@@ -41,10 +45,19 @@ def student_score(marks):
 
 marks = 25
 exam_result = student_score(marks)
-print("Student score pass mark:?",not exam_result)
 print("Student score fail mark:?", exam_result)
 print("_______________________________________________")
 
+def centum(marks):
+    full_mark = marks == 100
+    return full_mark
+
+score = 100
+testscore = centum(score)
+print("Has student scored centum in exam:?",testscore)
+print("_______________________________________________")
+
+# Given body temperature check if user has fever(more than 100)
 def can_vacinate(temp):
     fever = temp >= 100
     return fever
@@ -53,6 +66,29 @@ temp = 95
 eligible_for_shot = can_vacinate(temp)
 print("Eligible for shot:? ", not eligible_for_shot)
 
+# Given username, password check whether they match 'admin' '1234'
+
+def match(user_name,pswd):
+    match_usercheck = user_name == "admin"
+    match_pswdcheck = pswd == "1234"
+    both_match = match_usercheck and  match_pswdcheck
+    print ("Entered details has been matched :")
+
+get_userdetails = input("Enter user name : ")
+get_pswddetails = input("Enter password details: ")
+match_check = match(get_userdetails,get_pswddetails)
 
 
-    
+def check_num(number)  :
+    postive_num = number > 0
+    print("Entered number is postive: ",postive_num)  
+    negative_num = number < 0
+    print("Entered number is negative: ",negative_num)
+    equal_num = number == 0
+    print("Entered number is equal to zero: ",equal_num)
+    even_num = number % 2 == 0
+    print("Entered number is even: ",even_num)
+    print("Entered number is odd: ", not even_num)
+
+number = int(input("Enter a number: "))
+check_num(number)
